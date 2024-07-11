d5d6d85a0d4a1c54a590b41ca350f1e69f0c3da4
JS3322 <42951920+JS3322@users.noreply.github.com>
2024-07-11 13:39:59 +0900

    select_query.sql 업데이트

---
+++ select_query.sql
@@ -0,0 +1,31 @@
+db.collection.find({
+  $expr: {
+    $function: {
+      body: function(obj) {
+        function search(value, data) {
+          if (typeof data === 'object' && data !== null) {
+            if (Array.isArray(data)) {
+              for (let item of data) {
+                if (search(value, item)) return true;
+              }
+            } else {
+              for (let key in data) {
+                if (data.hasOwnProperty(key)) {
+                  if (data[key] === value) {
+                    return true;
+                  } else if (typeof data[key] === 'object' && data[key] !== null) {
+                    if (search(value, data[key])) return true;
+                  }
+                }
+              }
+            }
+          }
+          return false;
+        }
+        return search(90, obj);
+      },
+      args: ["$$ROOT"],
+      lang: "js"
+    }
+  }
+});
